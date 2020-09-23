8c697649feabf93e07d97708ffb1dcb1885e09b6
karynclarke <karynclarke@comcast.net>
2020-09-23 09:54:59 -0700

    service worker updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,53 @@
-var CACHE_NAME = "static_cache";
-// install
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
+var urlsToCache = ["/", "/db.js", "/index.js", "/manifest.json", "/styles.css", "/icons/icon-192x192.png", "/icons/icon-512x512.png"]
+    // var CACHE_NAME = "static_cache";
+
 self.addEventListener("install", function(evt) {
+    // pre cache image data
     evt.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            return cache.addAll(urlsToCache);
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("open cache");
+            return cache.addAll(urlsToCache)
         })
     );
-    self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(evt) {
-    // console.log("[service worker] fetching something: ", evt);
-    evt.respondWith(fetch(evt.request));
-});
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone())
+                    }
+                    return response
+                }).catch(err => {
+                    return cache.match(event.request)
+                });
+            }).catch(err => console.log(err))
 
-var promise = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        resolve("execution done");
-        //reject({ code: 500, message: "error happened" });
-    }, 3000);
-});
+        )
+        return;
+    }
 
-promise.then(function(value) {
-    console.log(value);
-}).catch(function(error) {
-    console.log(error.code, error.message);
-}).then(function(value) {
-    console.log(value);
-});
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+})
 
-fetch("https://httpbin.org/ip").then(function(response) {
-        console.log("response: ", response);
-        return response.json();
-    })
-    .then(function(data) { console.log(data); })
-    .catch(function(error) { console.log(error); });
+
+
+// tell the browser to activate this service worker immediately once it
+// has finished installing
